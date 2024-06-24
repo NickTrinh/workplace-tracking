cb5e991688f21021f2e59f6430e25d38a3fc1bd7
NickTrinh <nhattvm1112@gmail.com>
2024-06-24 00:53:18 -0400

    added mask and line flashes green when worker cross

--- ultralytics_tracking.py
+++ ultralytics_tracking.py
@@ -14,7 +14,8 @@ video_path_out = '{}_out.mp4'.format(os.path.splitext(video_path)[0]) # output v
 cap = cv2.VideoCapture(video_path) # in video
 ret, frame = cap.read()
 H, W, _ = frame.shape
-out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H)) # out video
+out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), 
+                    int(cap.get(cv2.CAP_PROP_FPS)), (W, H)) # out video
 
 direction={}
 direction_counter=set()
@@ -24,26 +25,27 @@ def boxes_overlap(box1, box2):
     x_min1, y_min1, x_max1, y_max1 = box1
     x_min2, y_min2, x_max2, y_max2 = box2
     
-    x_mid2 = (x_min2 + x_max2)/2
-    y_mid2 = (y_min2 + y_max2)/2
-    
-    return not (x_max1 < (x_min2 + x_mid2)/2 or (x_max2 + x_mid2)/2 < x_min1 or y_max1 < (y_min2 + y_mid2)/2 
-                or (y_max2 + y_mid2)/2 < y_min1)
+    return not (x_max1 < x_min2 or x_max2 < x_min1 or y_max1 < y_min2 or y_max2 < y_min1)
+
+mask = cv2.imread('videos/mask.png')
 
 while cap.isOpened():
     # Read frame from video
     ret, frame = cap.read()
     
     if ret:
+        imgRegion = cv2.bitwise_and(frame, mask)
         # Run YOLOv8 tracking on the frame
-        results = model.track(frame, persist=True, tracker='bytetrack.yaml', imgsz=1920, device='cuda:0', conf=0.2)
+        results = model.track(imgRegion, persist=True, tracker='botsort.yaml', imgsz=1280, 
+                            device='cuda:0', iou=0.3, conf=0.2)
         
         # Visualize results on the frame
-        annotated_frame = results[0].plot()
+        annotated_frame = results[0].plot(img=frame)
+        
         
         line_y = 300
         x_start, x_end = 550, 850
-        cv2.line(annotated_frame,(x_start,line_y),(x_end,line_y),(255,255,255),2)
+        cv2.line(annotated_frame,(x_start,line_y),(x_end,line_y),(0,0,255),2)
         
         boxes = results[0].boxes.xyxy.cpu().numpy()
         class_ids = results[0].boxes.cls.cpu().numpy()
@@ -72,17 +74,19 @@ while cap.isOpened():
             # Ensure track_id exists in direction
             if track_id not in direction:
                 direction[track_id] = 'not_tested'  # Initialize with a default status
-
+            
             
             if direction[track_id] == 'tested':
-                cv2.putText(annotated_frame, "Tested", (center_x, center_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(annotated_frame, "Tested", (center_x, center_y), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             
             if line_y < (center_y + 7) and line_y > (center_y - 7) and  int(class_id) == 2:
                 direction[track_id] = center_y
                 
                 if track_id in direction:
                     direction_counter.add(track_id)
-        
+                    cv2.line(annotated_frame,(x_start,line_y),(x_end,line_y),(0,255,0),2)
+            
         for box2, track_id2 in class_2_boxes:
             for box0, _ in class_0_boxes:
                 if boxes_overlap(box2, box0):
@@ -95,7 +99,8 @@ while cap.isOpened():
                     break  # Assuming only one breathalyzer test is needed per worker
         
         worker_count = (len(direction_counter))
-        cv2.putText(annotated_frame, "Worker count: " + str(worker_count), (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        cv2.putText(annotated_frame, "Worker count: " + str(worker_count), (10,30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         # Display the frame with the annotated results
         out.write(annotated_frame)
     else:
