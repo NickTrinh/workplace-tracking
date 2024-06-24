811aa6a21ddf93f1f78956bde18c583401aff87f
NickTrinh <nhattvm1112@gmail.com>
2024-06-24 00:52:22 -0400

    better model training. takes longer to train

--- train_model.py
+++ train_model.py
@@ -8,7 +8,7 @@ def run():
     model.to(device) #Use GPU
     
     model.train(data="D:/projects/workplace-tracking/data/data.yaml", batch=-1, epochs=300, patience=30, 
-                imgsz=1920, cos_lr=True)  # train the model
+                imgsz=1280, cos_lr=True)  # train the model
 
 if __name__ == '__main__':
     run()
