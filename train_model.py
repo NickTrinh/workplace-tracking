f91012b08efaaa2066007a752855d6af7d171a6e
NickTrinh <nhattvm1112@gmail.com>
2024-06-24 00:51:38 -0400

    better training results. takes longer

--- train_model.py
+++ train_model.py
@@ -4,11 +4,11 @@ import torch
 def run():
     # Check if CUDA is available
     device = torch.device('cuda')
-    model = YOLO("yolov8n.pt").load("workerguardmodel.pt")
+    model = YOLO("yolov8x.pt").load("workerguardmodel.pt")
     model.to(device) #Use GPU
     
     model.train(data="D:/projects/workplace-tracking/data/data.yaml", batch=-1, epochs=300, patience=30, 
-                imgsz=1280, cos_lr=True)  # train the model
+                imgsz=1920, cos_lr=True)  # train the model
 
 if __name__ == '__main__':
     run()
