21ffd26ef2155030b2a57792aac2dd787a97ef66
NickTrinh <nhattvm1112@gmail.com>
2024-06-26 03:29:02 -0400

    minor changes to training

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ def run():
     # Check if CUDA is available
     model = YOLO("yolov8s.pt").load("workerguardmodel.pt")
     
-    model.train(data="D:/projects/workplace-tracking/data/data.yaml", batch=-1, epochs=300, patience=30, 
+    model.train(data="D:/projects/workplace-tracking/data/data.yaml", batch=-1, epochs=300, patience=50, 
                 imgsz=1280, cos_lr=True, amp=True, save=True, save_period=50, device=0)  # train the model
 
 if __name__ == '__main__':
