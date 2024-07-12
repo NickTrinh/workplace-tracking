5c7c11044acd379f3aa24bacb4b6c489fd355c95
NickTrinh <nhattvm1112@gmail.com>
2024-07-12 09:40:18 +0700

    changed to using yolov8-p2 models for small objects

--- train_model.py
+++ train_model.py
@@ -1,11 +1,10 @@
 from ultralytics import YOLO
-import torch
 
 def run():
-    model = YOLO("yolov8m.pt")
+    model = YOLO("yolov8n-p2.yaml").load("workerguardmodel.pt")
     
-    model.train(data="D:/projects/workplace-tracking/data/data.yaml", batch=-1, epochs=900, patience=90, 
-                imgsz=1280, hsv_h=0, hsv_v=0, hsv_s=0, scale=0, shear=0, translate=0, device=0, amp=True, cos_lr=True)
+    model.train(data="D:/projects/workplace-tracking/data/data.yaml", batch=-1, epochs=600, patience=60, 
+                imgsz=1280, cos_lr=True, exist_ok=True, device=0)
 
 if __name__ == '__main__':
     run()
